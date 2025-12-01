3b03e77ba5a58be59e31c94c4a1eb353326c2b36
RubaZyada <robazyada167@gmail.com>
2025-12-01 16:16:39 +0200

    splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,22 @@
-
 import 'package:api_ecommerce_app/config/cache/cache_constants.dart';
 import 'package:api_ecommerce_app/config/cache/cache_helper.dart';
 import 'package:api_ecommerce_app/config/router/app_router.dart';
 import 'package:api_ecommerce_app/config/router/routes.dart';
 import 'package:api_ecommerce_app/core/di.dart';
+//import 'package:api_ecommerce_app/generated/l10n.dart';
 import 'package:flutter/material.dart';
 import 'dart:developer';
+
+//import 'package:flutter_localizations/flutter_localizations.dart';
+
 void main() async {
   setupDependencyInjection();
-   WidgetsFlutterBinding.ensureInitialized();
+  WidgetsFlutterBinding.ensureInitialized();
   CacheHelper.init();
-   final String ? accessToken=await CacheHelper.getSecureData(key: CacheConstants.accessToken);
-   log(accessToken?? "null");
+  final String? accessToken = await CacheHelper.getSecureData(
+    key: CacheConstants.accessToken,
+  );
+  log(accessToken ?? "null");
   runApp(const MyApp());
 }
 
@@ -20,6 +25,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      // localizationsDelegates: [
+      //   S.delegate,
+      //   GlobalMaterialLocalizations.delegate,
+      //   GlobalWidgetsLocalizations.delegate,
+      //   GlobalCupertinoLocalizations.delegate,
+      // ],
+      // supportedLocales: S.delegate.supportedLocales,
       debugShowCheckedModeBanner: false,
       initialRoute: RoutesName.login,
       onGenerateRoute: AppRouter().onGenerateRoute,
